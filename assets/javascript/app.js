40c4be15066f82ff623823923c73acaa1d0bb90c
Stu Eliades <stuart.eliades@gmail.com>
2019-06-18 23:48:08 -0400

    attempted to finish game mechanics but cannot get countdown to function correctly and checkAnswer function is off by a click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,96 @@
+var questions = [
+    {
+        question: "What do you call a group of gorillas?",
+        imgSrc: "./assets/images/gorilla.jpg",
+        optionA: "A band",
+        optionB: "A bunch",
+        optionC: "A kong",
+        optionD: "A gaggle",
+        correct: optionA,
+    },
+    {
+        question: "What do you call a group of stingrays?",
+        imgSrc: "./assets/images/stingray.jpg",
+        optionA: "A glide",
+        optionB: "A school",
+        optionC: "A fever",
+        optionD: "A flight",
+        correct: optionC,
+    },
+    {
+        question: "What do you call a group of cobras?",
+        imgSrc: "./assets/images/cobra.png",
+        optionA: "A basket",
+        optionB: "A quiver",
+        optionC: "A nightmare",
+        optionD: "A slither",
+        correct: optionB,
+    },
+    {
+        question: "What do you call a group of toads?",
+        imgSrc: "./assets/images/toad.jpg",
+        optionA: "A plague",
+        optionB: "A knot",
+        optionC: "A council",
+        optionD: "A croak",
+        correct: optionB,
+    },
+    {
+        question: "What do you call a group of bears?",
+        imgSrc: "./assets/images/bear.jpg",
+        optionA: "A bevy",
+        optionB: "A baloo",
+        optionC: "A troop",
+        optionD: "A sleuth",
+        correct: optionD,
+    },
+    {
+        question: "What do you call a group of ferrets?",
+        imgSrc: "./assets/images/ferrets.png",
+        optionA: "A tangle",
+        optionB: "A colony",
+        optionC: "A business",
+        optionD: "A flock",
+        correct: optionC,
+    },
+    {
+        question: "What do you call a group of skunks?",
+        imgSrc: "./assets/images/skunk.jpg",
+        optionA: "A stench",
+        optionB: "A rankness",
+        optionC: "A squad",
+        optionD: "An odor ",
+        correct: optionA,
+    },
+    {
+        question: "What do you call a group of parrots?",
+        imgSrc: "./assets/images/parrot.jpg",
+        optionA: "A cacophony",
+        optionB: "A pulchritude",
+        optionC: "A discord",
+        optionD: "A pandemonium",
+        correct: optionD,
+    },
+    {
+        question: "What do you call a group of lemurs?",
+        imgSrc: "./assets/images/lemur.png",
+        optionA: "A league",
+        optionB: "A conspiracy",
+        optionC: "A shadow",
+        optionD: "A leap",
+        correct: optionB,
+    },
+    {
+        question: "What do you call a group of porcupines?",
+        imgSrc: "./assets/images/porcupine.png",
+        optionA: "A prickle",
+        optionB: "A poke",
+        optionC: "A cuddle",
+        optionD: "A puddle",
+        correct: optionA,
+    }
+];
+
 var startScreen = document.getElementById("start-screen");
 var quiz = document.getElementById("quiz");
 var animalImg = document.getElementById("animal-img");
@@ -9,103 +102,11 @@ var optionC = document.getElementById("C");
 var optionD = document.getElementById("D");
 var endScreen = document.getElementById("end-screen");
 
-var questions = [
-    {
-        question: "What do you call a group of gorillas?",
-        imgSrc: "./assets/images/gorilla.jpg",
-        optionA: "A band",
-        optionB: "A bunch",
-        optionC: "A kong",
-        optionD: "A gaggle",
-        correct: "A",
-    },
-    {
-        question: "What do you call a group of stingrays?",
-        imgSrc: "./assets/images/stingray.jpg",
-        optionA: "A glide",
-        optionB: "A school",
-        optionC: "A fever",
-        optionD: "A flight",
-        correct: "C",
-    },
-    {
-        question: "What do you call a group of cobras?",
-        imgSrc: "./assets/images/cobra.png",
-        optionA: "A basket",
-        optionB: "A quiver",
-        optionC: "A nightmare",
-        optionD: "A slither",
-        correct: "B",
-    },
-    {
-        question: "What do you call a group of toads?",
-        imgSrc: "./assets/images/toad.jpg",
-        optionA: "A plague",
-        optionB: "A knot",
-        optionC: "A council",
-        optionD: "A croak",
-        correct: "B",
-    },
-    {
-        question: "What do you call a group of bears?",
-        imgSrc: "./assets/images/bear.jpg",
-        optionA: "A bevy",
-        optionB: "A baloo",
-        optionC: "A troop",
-        optionD: "A sleuth",
-        correct: "D",
-    },
-    {
-        question: "What do you call a group of ferrets?",
-        imgSrc: "./assets/images/ferrets.png",
-        optionA: "A tangle",
-        optionB: "A colony",
-        optionC: "A business",
-        optionD: "A flock",
-        correct: "C",
-    },
-    {
-        question: "What do you call a group of skunks?",
-        imgSrc: "./assets/images/skunk.jpg",
-        optionA: "A stench",
-        optionB: "A rankness",
-        optionC: "A squad",
-        optionD: "An odor ",
-        correct: "A",
-    },
-    {
-        question: "What do you call a group of parrots?",
-        imgSrc: "./assets/images/parrot.jpg",
-        optionA: "A cacophony",
-        optionB: "A pulchritude",
-        optionC: "A discord",
-        optionD: "A pandemonium",
-        correct: "D",
-    },
-    {
-        question: "What do you call a group of lemurs?",
-        imgSrc: "./assets/images/lemur.png",
-        optionA: "A league",
-        optionB: "A conspiracy",
-        optionC: "A shadow",
-        optionD: "A leap",
-        correct: "B",
-    },
-    {
-        question: "What do you call a group of porcupines?",
-        imgSrc: "./assets/images/porcupine.png",
-        optionA: "A prickle",
-        optionB: "A poke",
-        optionC: "A cuddle",
-        optionD: "A puddle",
-        correct: "A",
-    }
-];
 
 var lastQuestion = questions.length - 1;
 var runningQuestion = 0;
 var score = 0;
-var count = 10;
+var count = 8;
 var timer;
 
 function renderQuestion() {
@@ -118,7 +119,7 @@ function renderQuestion() {
     optionD.innerHTML = q.optionD;
 };
 
-startScreen.addEventListener("click", startQuiz);
+$(startScreen).on("click", startQuiz);
 
 function startQuiz() {
     startScreen.style.display = "none";
@@ -152,19 +153,21 @@ function answerIsRight() { };
 function answerIsWrong() { };
 
 function renderScore() {
+    quiz.style.display = "none";
     endScreen.style.display = "block";
-    var scorePercent = Math.round(100 * score / questions.length);
-    endScreen.innerHTML = "<p> You got " + scorePercent + " % correct! </p>";
+    endScreen.innerHTML = "<p> You got " + score + " correct! </p>";
+    setInterval(startQuiz, 5000);
 };
 
-function checkAnswer(answer) {
+$(document).on("click", ".option", checkAnswer)
+function checkAnswer() {
+    var answer = $(this).text();
     if (answer == questions[runningQuestion].correct) {
         score++;
         answerIsCorrect();
     } else {
         answerIsWrong();
     }
-    count = 10;
     if (runningQuestion < lastQuestion) {
         runningQuestion++;
         renderQuestion();
